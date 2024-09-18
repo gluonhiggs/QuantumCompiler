a76ed5d2c5bb093d58dba52ff693b45eddede73f
gluonhiggs <love.fiziks@gmail.com>
2024-09-18 14:22:31 +0000

    modified container

--- test.py
+++ test.py
@@ -1,11 +1,22 @@
-import torch
+import numpy as np
+def rotation_gate(axis, angle):
+    if axis == 'x':
+        return np.array([[np.cos(angle / 2), -1j * np.sin(angle / 2)],
+                         [-1j * np.sin(angle / 2), np.cos(angle / 2)]], dtype=complex)
+    elif axis == 'y':
+        return np.array([[np.cos(angle / 2), -np.sin(angle / 2)],
+                         [np.sin(angle / 2), np.cos(angle / 2)]], dtype=complex)
+    elif axis == 'z':
+        return np.array([[np.exp(-1j * angle / 2), 0],
+                         [0, np.exp(1j * angle / 2)]], dtype=complex)
 
-# Check if CUDA (GPU) is available
-if torch.cuda.is_available():
-    device = torch.device("cuda")
-    print("Using CUDA device:", torch.cuda.get_device_name(0))
-else:
-    device = torch.device("cpu")
-    print("Using CPU")
-
-
+# Gate set B
+my_gate_set = [
+    rotation_gate('x', np.pi / 128),
+    rotation_gate('x', -np.pi / 128),
+    rotation_gate('y', np.pi / 128),
+    rotation_gate('y', -np.pi / 128),
+    rotation_gate('z', np.pi / 128),
+    rotation_gate('z', -np.pi / 128)
+]
+print(my_gate_set)
